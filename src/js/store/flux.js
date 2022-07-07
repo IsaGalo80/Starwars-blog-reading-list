e623b40ba9d9d769324c15e3ba43da944c5a47e9
IsaGalo80 <isagalo1980@outlook.com>
2022-07-07 08:45:40 +0000

    falta hacer

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,33 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+				people:[],
+				planets:[],
+			
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+		personajes: () => {
+				
+						fetch("https://www.swapi.tech/api/people/")
+						.then(res => res.json())
+						.then(data => setStore({people:data.results}))
+						.catch(err => console.error(err))		
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			planetas: () => {
+				fetch("https://www.swapi.tech/api/planets/")
+				.then(res => res.json())
+				.thenthen(data => setStore({planets:data.results}))
+				.catch(err => console.error(err))
+			},
+			vehiculos: () => {
+				fetch("https://www.swapi.tech/api/vehicles/")
+				.then(res => res.json())
+				.thenthen(data => setStore({vehiculos:data.results}))
+				.catch(err => console.error(err))
 			}
 		}
 	};
